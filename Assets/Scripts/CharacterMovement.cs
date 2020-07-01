104859668aecb3cc4dd862a305839862abe91b6b
Jann Lüllmann <jann.luellmann@web.de>
2020-07-01 18:29:15 +0200

    Started implementing movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+
+    [SerializeField] public Rouguelite controls;
+    private Animator animator;
+
+    private void Awake()
+    {
+        this.animator = GetComponent<Animator>();
+
+        controls.Player.Move.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector2 direction)
+    {
+        transform.position += new Vector3(direction.x * speed * Time.deltaTime, direction.y * speed * Time.deltaTime, 0f);
+    }
+}
